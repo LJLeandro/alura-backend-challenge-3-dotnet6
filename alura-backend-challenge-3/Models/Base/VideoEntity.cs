a6234fd7db83738e4bb5c3bf8f6bee039b12e9b8
Lucas Leandro <lucasjleandro@gmail.com>
2022-10-24 10:23:13 -0300

    Add project files.

---
+++ alura-backend-challenge-3/Models/Base/VideoEntity.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace alura_backend_challenge_3.Models.Base
+{
+    [Table("videos")]
+    public class VideoEntity
+    {
+        [Column("titulo")]
+        [Required]
+        [StringLength(50)]
+        public string Titulo { get; set; }
+
+        [Column("descricao")]
+        [Required]
+        [StringLength(200)]
+        public string Descricao { get; set; }
+
+        [Column("url")]
+        [StringLength(100)]
+        public string URL { get; set; }
+    }
+}
---
+++ alura-backend-challenge-3/Models/Context/MySQLContext.cs
@@ -0,0 +1,12 @@
+﻿using alura_backend_challenge_3.Models.Base;
+using Microsoft.EntityFrameworkCore;
+
+namespace alura_backend_challenge_3.Models.Context
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base (options) { }
+
+        public DbSet<VideoEntity> Videos { get; set; }
+    }
+}
