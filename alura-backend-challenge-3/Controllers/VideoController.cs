a94790220b4b12bee90a2a4117914cc34193ffe5
Lucas Leandro <lucasjleandro@gmail.com>
2022-10-24 11:27:11 -0300

    O Endpoint para Criação de Registros foi implementado

---
+++ alura-backend-challenge-3/Controllers/VideoController.cs
@@ -0,0 +1,30 @@
+﻿using alura_backend_challenge_3.Data.ValueObjects;
+using alura_backend_challenge_3.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace alura_backend_challenge_3.Controllers
+{
+    [Route("api/v1/videos")]
+    [ApiController]
+    public class VideoController : ControllerBase
+    {
+        private IVideoRepository _repository;
+
+        public VideoController(IVideoRepository repository)
+        {
+            _repository = repository ??
+                throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(VideoVO videoVO)
+        {
+            if (videoVO == null) return BadRequest();
+
+            var product = await _repository.Create(videoVO);
+
+            return Ok(product);
+        }
+    }
+}
